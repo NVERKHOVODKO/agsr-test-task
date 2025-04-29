107ec73843a1df06c38b450e84923fecb09962e4
nikita_ve <nikita_ve@holdingbp.ru>
2025-04-29 18:55:17 +0300

    refactor: main - refactor program cs

--- Patient.API/Patient.API/Program.cs
+++ Patient.API/Patient.API/Program.cs
@@ -15,60 +15,83 @@ using Patient.Core.Services.Interfaces;
 using Serilog;
 using Serilog.Events;
 using Serilog.Formatting.Compact;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Patient.API;
 
-internal abstract class Program
+public static class Program
 {
     public static void Main(string[] args)
     {
+        var builder = CreateWebApplicationBuilder(args);
+        var app = BuildApplication(builder);
+        app.Run();
+    }
+
+    private static WebApplicationBuilder CreateWebApplicationBuilder(string[] args)
+    {
         var builder = WebApplication.CreateBuilder(args);
 
+        ConfigureLogging(builder);
+        ConfigureServices(builder);
+        ConfigureSwagger(builder);
+
+        return builder;
+    }
+
+    private static void ConfigureLogging(WebApplicationBuilder builder)
+    {
+        const string logsDirectory = "Logs";
+        Directory.CreateDirectory(logsDirectory);
+
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.Console()
+            .WriteTo.File(
+                new CompactJsonFormatter(),
+                Path.Combine(logsDirectory, $"information-{DateTime.UtcNow:yyyy-MM-dd}.log"),
+                rollingInterval: RollingInterval.Day,
+                shared: true)
+            .WriteTo.File(
+                new CompactJsonFormatter(),
+                Path.Combine(logsDirectory, $"error-{DateTime.UtcNow:yyyy-MM-dd}.log"),
+                LogEventLevel.Error,
+                rollingInterval: RollingInterval.Day,
+                shared:true)
+            .CreateLogger();
+
+        builder.Host.UseSerilog();
+    }
+
+    private static void ConfigureServices(WebApplicationBuilder builder)
+    {
         builder.Services.AddControllers()
             .AddJsonOptions(options =>
             {
                 options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
                 options.JsonSerializerOptions.WriteIndented = true;
             });
+
         builder.Services.AddDbContext<DataBaseContext>(options =>
-        {
-            options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty);
-        });
+            options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty));
 
         builder.Services.Configure<RouteOptions>(options =>
         {
             options.LowercaseUrls = true;
             options.LowercaseQueryStrings = true;
         });
-        
         builder.Services.AddScoped<IRepository<Core.Models.Patient>, PatientRepository>();
         builder.Services.AddScoped<IPatientService, PatientService>();
-        builder.Services.AddAutoMapper(typeof(PatientProfile));
         builder.Services.AddScoped<IDataHelper, DataHelper>();
-        
-        #region Logs
+        builder.Services.AddAutoMapper(typeof(PatientProfile));
+    }
 
-        var logsDirectory = Path.Combine("Logs");
-        if (!Directory.Exists(logsDirectory))
-            Directory.CreateDirectory(logsDirectory);
-
-        Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Information()
-            .WriteTo.Console()
-            .WriteTo.File(new CompactJsonFormatter(), $"Logs/information-{DateTime.UtcNow.ToShortDateString()}.log",
-                rollingInterval: RollingInterval.Day, shared: true)
-            .WriteTo.File(new CompactJsonFormatter(), $"Logs/error-{DateTime.UtcNow.ToShortDateString()}.log", LogEventLevel.Error,
-                rollingInterval: RollingInterval.Day, shared: true)
-            .CreateLogger();
-
-        builder.Host.UseSerilog();
-
-        #endregion
-
+    private static void ConfigureSwagger(WebApplicationBuilder builder)
+    {
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen(c =>
+        builder.Services.AddSwaggerGen(options =>
         {
-            c.SwaggerDoc("v1", new OpenApiInfo
+            options.SwaggerDoc("v1", new OpenApiInfo
             {
                 Title = "Patient API",
                 Version = "v1",
@@ -79,34 +102,50 @@ internal abstract class Program
                     Email = "mikita.verkhavodka@gmail.com"
                 }
             });
-
-            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-            c.IncludeXmlComments(xmlPath);
-            c.EnableAnnotations();
+            IncludeXmlComments(options);
+            options.EnableAnnotations();
         });
+    }
 
+    private static void IncludeXmlComments(SwaggerGenOptions options)
+    {
+        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+        options.IncludeXmlComments(xmlPath);
+    }
+
+    private static WebApplication BuildApplication(WebApplicationBuilder builder)
+    {
         var app = builder.Build();
-        app.UseMiddleware<ExceptionMiddleware>();
 
-        using (var scope = app.Services.CreateScope())
+        app.UseMiddleware<ExceptionMiddleware>();
+        EnsureDatabaseCreated(app);
+        ConfigureMiddlewarePipeline(app);
+
+        return app;
+    }
+
+    private static void EnsureDatabaseCreated(WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<DataBaseContext>();
+        dbContext.Database.EnsureCreated();
+    }
+
+    private static void ConfigureMiddlewarePipeline(WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
         {
-            var dbContext = scope.ServiceProvider.GetRequiredService<DataBaseContext>();
-            dbContext.Database.EnsureCreated();
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+                options.DisplayRequestDuration();
+            });
         }
 
         app.UseHttpsRedirection();
-
         app.UseAuthorization();
-
         app.MapControllers();
-
-        app.UseSwagger();
-        app.UseSwaggerUI(c =>
-        {
-            c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
-        });
-
-        app.Run();
     }
 }
